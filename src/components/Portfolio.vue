d14a2449e15f1b6db37bf84ec3adb3546b283ab0
Malikov Azizjon <80214840+MalikovAzizjon4843@users.noreply.github.com>
2025-05-28 18:11:52 +0500

    swiper added and fix

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,26 +1,26 @@
 <template>
-  <section class="rn-blog-area rn-section-gap section-separator" id="portfolio">
+  <section class="rn-portfolio-area rn-section-gap section-separator" id="portfolio">
     <div class="container">
       <div class="text-center mb-8">
-        <div data-aos="fade-up" data-aos-duration="500" data-aos-delay="100" data-aos-once="true" class="section-title text-center">
+        <div class="section-title text-center">
           <span class="subtitle">Visit my portfolio and keep your feedback</span>
           <h2 class="title">My Portfolio</h2>
         </div>
       </div>
 
       <swiper :options="swiperOptions" class="portfolio-swiper">
-        <swiper-slide v-for="(blog, index) in blogs" :key="index">
-          <div class="rn-blog">
+        <swiper-slide v-for="(item, index) in items" :key="index">
+          <div class="rn-portfolio">
             <div class="inner">
               <div class="thumbnail">
-                <img :src="require(`@/assets/images/blog/${blog.image}`)" :alt="blog.title" />
+                <img :src="require(`@/assets/images/portfolio/${item.image}`)" :alt="item.title" />
               </div>
               <div class="content">
                 <div class="category-info">
-                  <div class="category-list"><a href="#">{{ blog.category }}</a></div>
-                  <div class="meta"><span><i class="feather-clock"></i> {{ blog.readTime }}</span></div>
+                  <div class="category-list"><a href="#">{{ item.category }}</a></div>
+                  <div class="meta"><span><i class="feather-heart"></i> {{ item.likes }}</span></div>
                 </div>
-                <h4 class="title">{{ blog.title }}<i class="feather-arrow-up-right"></i></h4>
+                <h4 class="title">{{ item.title }} <i class="feather-arrow-up-right"></i></h4>
               </div>
             </div>
           </div>
@@ -30,71 +30,48 @@
         <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div>
       </swiper>
-
-      <!-- Modal -->
-      <div class="modal fade" id="exampleModalCenters" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-news" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true"><i data-feather="x"></i></span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <img :src="require(`@/assets/images/blog/${currentBlog.image}`)" alt="news modal" class="img-fluid modal-feat-img">
-              <div class="news-details">
-                <span class="date">{{ currentBlog.date }}</span>
-                <h2 class="title">{{ currentBlog.title }}</h2>
-                <p>{{ currentBlog.description }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
     </div>
   </section>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper';
-import 'swiper/css/swiper.css';
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
 
 export default {
   name: 'PortfolioSection',
   components: {
-    swiper: Swiper,
-    swiperSlide: SwiperSlide
+    swiper,
+    swiperSlide
   },
   data() {
     return {
-      blogs: [
+      items: [
         {
-          image: 'blog-01.jpg',
-          category: 'Canada',
-          readTime: '2 min read',
-          title: 'T-shirt design is the part of design',
-          date: '2 May, 2021',
-          description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit...'
+          image: 'portfolio-06.jpg',
+          category: 'Photoshop',
+          likes: 650,
+          title: 'The services provide for design'
         },
         {
-          image: 'blog-02.jpg',
-          category: 'Development',
-          readTime: '2 hour read',
-          title: 'The services provide for design',
-          date: '2 May, 2021',
-          description: 'Mauris tempor, orci id pellentesque convallis...'
+          image: 'portfolio-05.jpg',
+          category: 'Figma',
+          likes: 420,
+          title: 'Mobile app landing design & Services'
         },
         {
-          image: 'blog-03.jpg',
-          category: 'Application',
-          readTime: '5 min read',
-          title: 'Mobile app landing design & app maintain',
-          date: '2 May, 2021',
-          description: 'Ut rhoncus vestibulum facilisis...'
+          image: 'portfolio-04.jpg',
+          category: 'Laravel',
+          likes: 318,
+          title: 'Web app Responsive design & Services'
+        },
+        {
+          image: 'portfolio-03.jpg',
+          category: 'Figma',
+          likes: 210,
+          title: 'PHP with app landing design & Services'
         }
       ],
-      currentBlog: {},
       swiperOptions: {
         slidesPerView: 3,
         spaceBetween: 30,
@@ -114,17 +91,9 @@ export default {
           320: { slidesPerView: 1 }
         }
       }
-    };
-  },
-  methods: {
-    openBlog(blog) {
-      this.currentBlog = blog;
     }
-  },
-  created() {
-    this.currentBlog = this.blogs[0];
   }
-};
+}
 </script>
 
 <style scoped>
@@ -135,18 +104,17 @@ export default {
 .swiper-slide {
   display: flex;
   justify-content: center;
-  box-sizing: border-box;
 }
-.rn-blog {
-  max-width: 320px;
+.rn-portfolio {
+  max-width: 340px;
   width: 100%;
   background: #1e1e1e;
   padding: 20px;
-  border-radius: 8px;
+  border-radius: 10px;
   color: white;
 }
 .thumbnail img {
   width: 100%;
-  border-radius: 8px;
+  border-radius: 10px;
 }
 </style>
